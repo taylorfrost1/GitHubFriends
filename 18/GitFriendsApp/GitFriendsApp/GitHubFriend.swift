49f6e08a0ee3546f3c3c300bb9bebe9713bb530a
Taylor Frost <taylorhamblinfrost@gmail.com>
2016-06-21 15:05:15 -0600

    Just started the app

---
+++ 18/GitFriendsApp/GitFriendsApp/GitHubFriend.swift
@@ -0,0 +1,15 @@
+//
+//  GitHubFriend.swift
+//  GitFriendsApp
+//
+//  Created by Taylor Frost on 6/21/16.
+//  Copyright © 2016 Taylor Frost. All rights reserved.
+//
+
+import UIKit
+
+class GitHubFriend: NSObject {
+    
+    
+
+}
